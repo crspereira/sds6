431cbbd74ae2de4f337bb067c20d6fce0a3aa965
Clayton Pereira <clayton.pereira@outlook.com>
2022-01-14 16:39:51 -0300

    FrontEnd Project - First Request Basic Test

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
+import axios from "axios";
+import { BASE_URL } from "utils/request";
 
 function Listing() {
+
+	//TESTE DE REQUISIÇÃO
+	axios.get(`${BASE_URL}/movies?page=0&size=3`)
+	     .then(response => {
+				console.log(response.data);	
+		  });
+
 	return (
 		<>
 			<Pagination />
