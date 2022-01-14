e86ceef72f354864d4fc5f47fe1e02b7a8a1cf10
Clayton Pereira <clayton.pereira@outlook.com>
2022-01-14 17:13:44 -0300

    FrontEnd Project - Request into UseState and UseEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,17 +2,31 @@ import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
 import axios from "axios";
 import { BASE_URL } from "utils/request";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-	//TESTE DE REQUISIÇÃO
-	axios.get(`${BASE_URL}/movies?page=0&size=3`)
+	const [pageNumber, setPageNumber] = useState(0);
+
+	useEffect(() => {
+		axios.get(`${BASE_URL}/movies?page=1&size=3`)
 	     .then(response => {
-				console.log(response.data);	
+				const data = response.data as MoviePage
+				setPageNumber(data.number);	
 		  });
+	}, []);
+
+	//TESTE DE REQUISIÇÃO
+	/*axios.get(`${BASE_URL}/movies?page=1&size=3`)
+	     .then(response => {
+				const data = response.data as MoviePage
+				setPageNumber(data.number);	
+		  });*/
 
 	return (
 		<>
+			<p>{pageNumber}</p>
 			<Pagination />
 			<div className="container">
 				<div className="row">
